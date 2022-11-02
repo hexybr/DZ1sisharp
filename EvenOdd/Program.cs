080c636504fdf80bdeacb46ef3fe2410b956b7a9
Handbrake brake <handbrake@mail.ru>
2022-11-02 21:50:54 +0300

    ЧетноеНечетное

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число:");
+int x = int.Parse(Console.ReadLine()!);
+
+if ((x % 2) == 0)
+{
+Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
