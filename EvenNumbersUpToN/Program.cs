db539f8259d2314c8aa406326f3b5b3c60ac1496
Handbrake brake <handbrake@mail.ru>
2022-11-02 23:00:29 +0300

    Четный числа до N

---
+++ EvenNumbersUpToN/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число:");
+int N = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+
+while (count<=N)
+{
+    if ((count % 2) == 0)
+    {
+    Console.Write(count +",");
+    }
+count++;
+}
